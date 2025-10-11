a867bd74f7957bf76f258519ebdf53795ec65329
Buddhadeb Koner <jeetkoner36@gmail.com>
2025-10-11 13:30:06 +0530

    Add GetValueByKey class for retrieving values from HashMap and enhance main method to display search results

---
+++ 04Hashing/intro/src/com/buddhadeb/Iteration.java
@@ -0,0 +1,5 @@
+package com.buddhadeb;
+
+public class Iteration {
+   
+}
--- 04Hashing/intro/src/com/buddhadeb/Main.java
+++ 04Hashing/intro/src/com/buddhadeb/Main.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        //  country(key), populations(value)
+        // country(key), populations(value)
         HashMap<String, Integer> map = new HashMap<>();
 
         System.out.println(map);
@@ -16,5 +16,12 @@ public class Main {
         System.out.println("Returned map from Insert.insertOperations: " + updatedMap);
         // search India in the map
         Search.searchOparation(map, "India");
+        // get value by key
+        Integer value = GetValueByKey.getValue(map, "India");
+        if (value != null) {
+            System.out.print("India" + " is present in the map value is : " + value);
+        } else {
+            System.out.print("India" + " is not present in the map");
+        }
     }
 }
